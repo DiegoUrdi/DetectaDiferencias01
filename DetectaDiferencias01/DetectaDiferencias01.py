070884f2e01826878bd3ae011ae2f98d436e1353
DiegoUrdi <56819339+DiegoUrdi@users.noreply.github.com>
2021-06-20 09:53:29 +0200

    casa cuidao

--- DetectaDiferencias01/DetectaDiferencias01.py
+++ DetectaDiferencias01/DetectaDiferencias01.py
@@ -1,52 +1,141 @@
-# import the necessary packages
-from skimage.measure import compare_ssim
-import argparse
-import imutils
 import cv2
+import numpy as np
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--first", required=True,
-	help="first input image")
-ap.add_argument("-s", "--second", required=True,
-	help="second")
-args = vars(ap.parse_args())
+DEFAULT_TEMPLATE_MATCHING_THRESHOLD = 0.85
 
-for x in range(100):		
-	# load the two input images
-	imageA = cv2.imread(args["first"])
-	imageB = cv2.imread(args["second"])
 
-	# convert the images to grayscale
-	grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-	grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+class Template:
+    """
+    A class defining a template
+    """
+    def __init__(self, image_path, label, color, matching_threshold=DEFAULT_TEMPLATE_MATCHING_THRESHOLD):
+        """
+        Args:
+            image_path (str): path of the template image path
+            label (str): the label corresponding to the template
+            color (List[int]): the color associated with the label (to plot detections)
+            matching_threshold (float): the minimum similarity score to consider an object is detected by template
+                matching
+        """
+        self.image_path = image_path
+        self.label = label
+        self.color = color
+        self.template = cv2.imread(image_path)
+        self.template_height, self.template_width = self.template.shape[:2]
+        self.matching_threshold = matching_threshold
 
-	# compute the Structural Similarity Index (SSIM) between the two
-	# images, ensuring that the difference image is returned
-	(score, diff) = compare_ssim(grayA, grayB, full=True)
-	diff = (diff * 255).astype("uint8")
-	print("SSIM: {}".format(score))
+def compute_iou(boxA, boxB):
+   # determine the (x, y)-coordinates of the intersection rectangle
+   xA = max(boxA["TOP_LEFT_X"], boxB["TOP_LEFT_X"])
+   yA = max(boxA["TOP_LEFT_Y"], boxB["TOP_LEFT_Y"])
+   xB = min(boxA["BOTTOM_RIGHT_X"], boxB["BOTTOM_RIGHT_X"])
+   yB = min(boxA["BOTTOM_RIGHT_Y"], boxB["BOTTOM_RIGHT_Y"])
+   # compute the area of intersection rectangle
+   interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+   # compute the area of both the prediction and ground-truth
+   # rectangles
+   boxAArea = (boxA["BOTTOM_RIGHT_X"] - boxA["TOP_LEFT_X"] + 1) * (boxA["BOTTOM_RIGHT_Y"] - boxA["TOP_LEFT_Y"] + 1)
+   boxBArea = (boxB["BOTTOM_RIGHT_X"] - boxB["TOP_LEFT_X"] + 1) * (boxB["BOTTOM_RIGHT_Y"] - boxB["TOP_LEFT_Y"] + 1)
+   # compute the intersection over union by taking the intersection
+   # area and dividing it by the sum of prediction + ground-truth
+   # areas - the interesection area
+   iou = interArea / float(boxAArea + boxBArea - interArea)
+   # return the intersection over union value
+   return iou
 
-	# threshold the difference image, followed by finding contours to
-	# obtain the regions of the two input images that differ
-	thresh = cv2.threshold(diff, 0, 255,
-		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
 
-	# loop over the contours
-	for c in cnts:
-		# compute the bounding box of the contour and then draw the
-		# bounding box on both input images to represent where the two
-		# images differ
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-		cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+def non_max_suppression(
+    objects,
+    non_max_suppression_threshold=0.5,
+    score_key="MATCH_VALUE",
+):
+    """
+    Filter objects overlapping with IoU over threshold by keeping only the one with maximum score.
+    Args:
+        objects (List[dict]): a list of objects dictionaries, with:
+            {score_key} (float): the object score
+            {top_left_x} (float): the top-left x-axis coordinate of the object bounding box
+            {top_left_y} (float): the top-left y-axis coordinate of the object bounding box
+            {bottom_right_x} (float): the bottom-right x-axis coordinate of the object bounding box
+            {bottom_right_y} (float): the bottom-right y-axis coordinate of the object bounding box
+        non_max_suppression_threshold (float): the minimum IoU value used to filter overlapping boxes when
+            conducting non max suppression.
+        score_key (str): score key in objects dicts
 
-	# show the output images
-	# cv2.imshow("Original", imageA)
-	# cv2.imshow("Modified", imageB)
-	cv2.imshow("Diff", diff)
-	# cv2.imshow("Thresh", thresh)
-	cv2.waitKey(3000)
+    Returns:
+        List[dict]: the filtered list of dictionaries.
+    """
+    sorted_objects = sorted(objects, key=lambda obj: obj[score_key], reverse=True)
+    filtered_objects = []
+    for object_ in sorted_objects:
+        overlap_found = False
+        for filtered_object in filtered_objects:
+            iou = compute_iou(object_, filtered_object)
+            if iou > non_max_suppression_threshold:
+                overlap_found = True
+                break
+        if not overlap_found:
+            filtered_objects.append(object_)
+    return filtered_objects
+
+image = cv2.imread("data/printed-circuit.jpg")
+
+templates = [
+    Template(image_path="data/component1.jpg", label="1", color=(0, 0, 255)),
+    Template(image_path="data/component2.jpg", label="2", color=(0, 255, 0)),
+    Template(image_path="data/component3.jpg", label="3", color=(0, 191, 255)),
+]
+
+detections = []
+
+for template in templates:
+    print("Template ");
+    template_matching = cv2.matchTemplate(
+        template.template, image, cv2.TM_CCOEFF_NORMED
+    )
+
+    match_locations = np.where(template_matching >= template.matching_threshold)
+
+    for (x, y) in zip(match_locations[1], match_locations[0]):
+        match = {
+            "TOP_LEFT_X": x,
+            "TOP_LEFT_Y": y,
+            "BOTTOM_RIGHT_X": x + template.template_width,
+            "BOTTOM_RIGHT_Y": y + template.template_height,
+            "MATCH_VALUE": template_matching[y, x],
+            "LABEL": template.label,
+            "COLOR": template.color
+        }
+        print("Match ")
+        detections.append(match)
+
+NMS_THRESHOLD = 0.2
+detections = non_max_suppression(detections, non_max_suppression_threshold=NMS_THRESHOLD)
+
+image_with_detections = image.copy()
+
+for detection in detections:
+    print("Detection ", detection['LABEL'], detection["TOP_LEFT_X"])
+    cv2.rectangle(
+        image_with_detections,
+        (detection["TOP_LEFT_X"], detection["TOP_LEFT_Y"]),
+        (detection["BOTTOM_RIGHT_X"], detection["BOTTOM_RIGHT_Y"]),
+        detection["COLOR"],
+        2,
+    )
+    cv2.putText(
+        image_with_detections,
+        f"{detection['LABEL']} - {detection['MATCH_VALUE']}",
+        (detection["TOP_LEFT_X"] + 2, detection["TOP_LEFT_Y"] + 20),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        detection["COLOR"],
+        1,
+        cv2.LINE_AA,
+    )
+
+#cv2.imwrite(f"output/result.jpeg", image_with_detections)
+
+cv2.imwrite(f"output/result.jpeg", image_with_detections)
+cv2.imshow("image_with_detections", image_with_detections)
+cv2.waitKey(0)
--- DetectaDiferencias01/detector.bat
+++ DetectaDiferencias01/detector.bat
@@ -1,12 +1,12 @@
 @echo off
 echo 1
-start python DetectaDiferencias01.py --first a.bmp --second b.bmp
+python DetectaDiferencias01.py --first C:\Users\D\source\repos\DiegoUrdi\KinectImage\KinectImage\bin\Release\a.bmp --second C:\Users\D\source\repos\DiegoUrdi\KinectImage\KinectImage\bin\Release\b.bmp
 :while
 (
 TIMEOUT /T 1
 echo 2
-python DetectaDiferencias01.py --first a.bmp --second b.bmp
+python DetectaDiferencias01.py --first C:\Users\D\source\repos\DiegoUrdi\KinectImage\KinectImage\bin\Release\a.bmp --second C:\Users\D\source\repos\DiegoUrdi\KinectImage\KinectImage\bin\Release\b.bmp
 echo 3
-python DetectaDiferencias01.py --first a.bmp --second b.bmp
+python DetectaDiferencias01.py --first C:\Users\D\source\repos\DiegoUrdi\KinectImage\KinectImage\bin\Release\a.bmp --second C:\Users\D\source\repos\DiegoUrdi\KinectImage\KinectImage\bin\Release\b.bmp
 goto :while
 )
